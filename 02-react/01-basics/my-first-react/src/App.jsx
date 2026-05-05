0106744dbeca6f874d56a3ac04e840d7a0092619
amit39 <amitsonagra01@gmail.com>
2026-05-05 13:35:32 +0000

    feat: created first Header and Footer components

--- 02-react/01-basics/my-first-react/src/App.jsx
+++ 02-react/01-basics/my-first-react/src/App.jsx
@@ -1,8 +1,21 @@
-function App() {
+function Header() {
   return (
     <>
       <h1>Hello, Amit!</h1>
-      <p>My first react app</p>
+      <p>My firse react app</p>
+    </>
+  );
+}
+
+function Footer() {
+  return <p>Made by Amit</p>;
+}
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
     </>
   );
 }
