707c67f5c5931c15782c67fe7576232d9dddb37d
amit39 <amitsonagra01@gmail.com>
2026-05-07 08:42:45 +0000

    feat: useNavigate Done

--- 02-react/01-basics/my-first-react/src/pages/Contact.jsx
+++ 02-react/01-basics/my-first-react/src/pages/Contact.jsx
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 function Contact() {
+  const navigate = useNavigate();
   return (
     <>
       <h1>Contact Page</h1>
+      <input type="text" name="" id="" />
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Submit
+      </button>
     </>
   );
 }
